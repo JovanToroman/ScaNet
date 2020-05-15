44e5f1bd3d6e2cd24b92718ed7ecdd928e643299
Jovan Toroman <toromanj@gmail.com>
2020-05-15 21:04:36 +0200

    remove adversarial net remainings

--- models.py
+++ models.py
@@ -54,60 +54,22 @@ def resnet(input_image):
 
     return enhanced
 
-def adversarial(image_):
-
-    with tf.compat.v1.variable_scope("discriminator"):
-
-        conv1 = _conv_layer(image_, 48, 11, 4, batch_nn = False)
-        conv2 = _conv_layer(conv1, 128, 5, 2)
-        conv3 = _conv_layer(conv2, 192, 3, 1)
-        conv4 = _conv_layer(conv3, 192, 3, 1)
-        conv5 = _conv_layer(conv4, 128, 3, 2)
-        
-        flat_size = 128 * 7 * 7
-        conv5_flat = tf.reshape(conv5, [-1, flat_size])
-
-        W_fc = tf.Variable(tf.compat.v1.truncated_normal([flat_size, 1024], stddev=0.01))
-        bias_fc = tf.Variable(tf.constant(0.01, shape=[1024]))
-
-        fc = leaky_relu(tf.matmul(conv5_flat, W_fc) + bias_fc)
-
-        W_out = tf.Variable(tf.compat.v1.truncated_normal([1024, 2], stddev=0.01))
-        bias_out = tf.Variable(tf.constant(0.01, shape=[2]))
-
-        adv_out = tf.nn.softmax(tf.matmul(fc, W_out) + bias_out)
-    
-    return adv_out
 
 def weight_variable(shape, name):
 
     initial = tf.compat.v1.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial, name=name)
 
+
 def bias_variable(shape, name):
 
     initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial, name=name)
 
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def leaky_relu(x, alpha = 0.2):
-    return tf.maximum(alpha * x, x)
-
-def _conv_layer(net, num_filters, filter_size, strides, batch_nn=True):
-    
-    weights_init = _conv_init_vars(net, num_filters, filter_size)
-    strides_shape = [1, strides, strides, 1]
-    bias = tf.Variable(tf.constant(0.01, shape=[num_filters]))
-
-    net = tf.nn.conv2d(net, weights_init, strides_shape, padding='SAME') + bias   
-    net = leaky_relu(net)
-
-    if batch_nn:
-        net = _instance_norm(net)
-
-    return net
 
 def _instance_norm(net):
 
@@ -123,14 +85,3 @@ def _instance_norm(net):
 
     return scale * normalized + shift
 
-def _conv_init_vars(net, out_channels, filter_size, transpose=False):
-
-    _, rows, cols, in_channels = [i for i in net.get_shape()]
-
-    if not transpose:
-        weights_shape = [filter_size, filter_size, in_channels, out_channels]
-    else:
-        weights_shape = [filter_size, filter_size, out_channels, in_channels]
-
-    weights_init = tf.Variable(tf.compat.v1.truncated_normal(weights_shape, stddev=0.01, seed=1), dtype=tf.float32)
-    return weights_init
