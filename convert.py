de725a3f6bb1babcfa759171bcd0cc2737d68ab6
Jovan Toroman <toromanj@gmail.com>
2020-05-20 17:13:33 +0200

    Add larger patches

--- convert.py
+++ convert.py
@@ -32,7 +32,7 @@ def duplicate_originals(path, path_to, number_of_perspectives):
 
 def rename(path):
     try:
-        [os.rename(os.path.join(path, f), os.path.join(path, str(index) + '.jpg')) for index, f in
+        [os.rename(os.path.join(path, f), os.path.join(path, str(int(f.split('.')[0]) -20486) + '.jpg')) for index, f in
          enumerate(os.listdir(path)) if not f.startswith('.')]
     except Exception as e:
         print(e)
@@ -52,12 +52,16 @@ def check_if_image_is_white(image):
     pixels = img.getdata()  # get the pixels as a flattened sequence
     white_thresh = 130
     n_white = 0
+    black_thresh = 20
+    n_black = 0
     for pixel in pixels:
         if pixel > white_thresh:
             n_white += 1
+        if pixel < black_thresh:
+            n_black += 1
     n = len(pixels)
 
-    return (n_white / float(n)) > 0.95
+    return (n_white / float(n)) > 0.95 or (n_black / float(n)) > 0.95
 
 def extract_patches(path_scanned, path_originals, patch_dims):
     try:
@@ -65,14 +69,15 @@ def extract_patches(path_scanned, path_originals, patch_dims):
         for index, f in enumerate(sorted(os.listdir(path_scanned))):
             one_image = io.imread(os.path.join(path_scanned, f))
             two_image = io.imread(os.path.join(path_originals, f))
-            patches = image.extract_patches_2d(one_image, patch_dims, 100, 100)
-            patches2 = image.extract_patches_2d(two_image, patch_dims, 100, 100)
+            patches = image.extract_patches_2d(one_image, patch_dims, 200, 100)
+            patches2 = image.extract_patches_2d(two_image, patch_dims, 200, 100)
             for index2, patch in enumerate(patches):
-                if not (check_if_image_is_white(patch) or check_if_image_is_white(patches2[index2])):
-                    io.imsave(os.path.join(path_scanned, "patches", str(count) + '.jpg'), patch)
-                    io.imsave(os.path.join(path_originals, "patches", str(count) + '.jpg'), patches2[index2])
-                    count += 1
+                # if not (check_if_image_is_white(patch) or check_if_image_is_white(patches2[index2])):
+                io.imsave(os.path.join(r'C:\Users\Jovan\Documents\Master_rad\DPED\dped\test\training_data\test', str(count) + '.jpg'), patch)
+                io.imsave(os.path.join(r'C:\Users\Jovan\Documents\Master_rad\DPED\dped\test\training_data\canon', str(count) + '.jpg'), patches2[index2])
+                count += 1
     except Exception as e:
+        print(e)
         return
 
 def align_images(path_originals, path_scanned, output_path):
@@ -88,12 +93,13 @@ def extract_sheets_of_paper(path_originals, path_transformed):
 
 
 
-extract_patches(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals - Copy',
-                r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_s10e\high_lighting_aligned', [100,100])
-# align_images(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals - Copy - Copy',
-#              r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_s10e\high_lighting_processed - Copy (2)',
-#              r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_s10e\high_lighting_aligned2')
-# rename(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals')
+extract_patches(r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_s10e\low_lighting_aligned_good',
+                r'C:\Users\Jovan\Documents\Master_rad\zajem\originals2 - Copy - Copy',
+                [100,100])
+# align_images(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals2 - Copy',
+#              r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_tatin\low_lighting_processed',
+#              r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_tatin\low_lighting_aligned')
+# rename(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals2 - Copy - Copy\patches')
 # extract_sheets_of_paper(r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_tatin\high_lighting',
 #              r'C:\Users\Jovan\Documents\Master_rad\zajem\samsung_tatin\high_lighting_processed')
-# resize(r'C:\Users\Jovan\Documents\Master_rad\zajem\originals - Copy', [1080, 1528])
+# resize(r'C:\Users\Jovan\Documents\Master_rad\DPED\dped\test\test_data\patches\test', [1080, 1528])
