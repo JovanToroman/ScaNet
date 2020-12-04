6756d041e762818e1b3060b98693b1f2b679d884
jovan <toromanj@gmail.com>
2020-12-04 17:07:04 +0000

    Remove unnecessary files

--- utils.py
+++ utils.py
@@ -3,13 +3,6 @@ import PIL
 from PIL import Image
 import cv2
 
-### Resnet
-import scipy.stats as st
-import tensorflow as tf
-import sys
-
-from functools import reduce
-
 
 def calc_mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
@@ -51,25 +44,3 @@ def resize_image_if_uneven_width_or_height(image):
     while height % 16 != 0:
         height -= 1
     return cv2.rotate(cv2.resize(image, (height, width)), rotateCode=cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
-
-### used for Resnet
-
-def log10(x):
-  numerator = tf.compat.v1.log(x)
-  denominator = tf.compat.v1.log(tf.constant(10, dtype=numerator.dtype))
-  return numerator / denominator
-
-def _tensor_size(tensor):
-    from operator import mul
-    return reduce(mul, (d for d in tensor.get_shape()[1:]), 1)
-
-def gauss_kernel(kernlen=21, nsig=3, channels=1):
-    interval = (2*nsig+1.)/(kernlen)
-    x = np.linspace(-nsig-interval/2., nsig+interval/2., kernlen+1)
-    kern1d = np.diff(st.norm.cdf(x))
-    kernel_raw = np.sqrt(np.outer(kern1d, kern1d))
-    kernel = kernel_raw/kernel_raw.sum()
-    out_filter = np.array(kernel, dtype = np.float32)
-    out_filter = out_filter.reshape((kernlen, kernlen, 1, 1))
-    out_filter = np.repeat(out_filter, channels, axis = 2)
-    return out_filter
